2d93ec2bec847ab769ff682d17d57e40c54d6ba7
t-mario-y <t.mario.yoshi@gmail.com>
2021-06-25 03:49:24 +0900

    feat: add AWS VPC resources

---
+++ src/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.0.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.46.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
---
+++ src/vpc.tf
@@ -0,0 +1,67 @@
+resource "aws_vpc" "secure_networking" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "secure-networking"
+  }
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = aws_vpc.secure_networking.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_c" {
+  vpc_id            = aws_vpc.secure_networking.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Name = "public-subnet-c"
+  }
+}
+
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = aws_vpc.secure_networking.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.11.0/24"
+  tags = {
+    Name = "private-subnet-a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_c" {
+  vpc_id            = aws_vpc.secure_networking.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.12.0/24"
+  tags = {
+    Name = "private-subnet-c"
+  }
+}
+
+resource "aws_internet_gateway" "secure_network" {
+  vpc_id = aws_vpc.secure_networking.id
+  tags = {
+    Name = "igw-for-secure-network"
+  }
+}
+
+resource "aws_route_table" "secure_network" {
+  vpc_id = aws_vpc.secure_networking.id
+  tags = {
+    Name = "route-table-for-secure-network"
+  }
+}
+
+resource "aws_route" "route_to_igw" {
+  route_table_id         = aws_route_table.secure_network.id
+  gateway_id             = aws_internet_gateway.secure_network.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "between_igw_route_and_public_subnet_a" {
+  route_table_id = aws_route_table.secure_network.id
+  subnet_id      = aws_subnet.public_subnet_a.id
+}
