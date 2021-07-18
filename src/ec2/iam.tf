9233246e1dbc1b1276a2223e4a7d9508a1817a7e
t-mario-y <t.mario.yoshi@gmail.com>
2021-07-18 16:39:50 +0900

    feat: modify IAM policy for EC2 IAM role
    "AmazonSSMFullAccess" is excessive for the purpose of logging in with SSM

--- src/ec2/iam.tf
+++ src/ec2/iam.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_role_policy_attachment" "ssm_full_access" {
+resource "aws_iam_role_policy_attachment" "ssm_login" {
   role       = aws_iam_role.ec2.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role" "ec2" {
